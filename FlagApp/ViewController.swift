8db5f904d21aa7e5d95ed80cb318962841dbff6c
Adilet Kenesbekov <bbddbbd2003@gmail.com>
2024-10-12 19:54:23 +0500

    Initial Commit

---
+++ FlagApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlagApp
+//
+//  Created by Adilet Kenesbekov on 12.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
